5c4acae48681823c879dac01f9615d4d5018d6ed
Roman Gaitbaev <romzesrover@gmail.com>
2022-12-10 15:07:30 +0500

    #10: Implement DI (Hilt) in order to work with repository and stuff in ViewModels

--- app/src/main/java/com/example/kodetrainee/App.kt
+++ app/src/main/java/com/example/kodetrainee/App.kt
@@ -2,10 +2,12 @@ package com.example.kodetrainee
 
 import android.app.Application
 import com.example.kodetrainee.network.api.ApiNetworkRetrofit
+import dagger.hilt.android.HiltAndroidApp
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
+@HiltAndroidApp
 class App: Application() {
     lateinit var apiNetworkRetrofit: ApiNetworkRetrofit
 
